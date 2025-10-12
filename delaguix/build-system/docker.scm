0440d2b0fb42a9f3cbbe636cf2dffec4a1080bc8
Michael Delago <michaeldelago98@gmail.com>
2025-10-12 17:26:57 -0400

    feat: add Guix build system for Docker/OCI containers

---
+++ delaguix/build-system/docker.scm
@@ -0,0 +1,110 @@
+;;; docker.scm --- Guix build system for Docker/OCI containers
+
+(define-module (delaguix build-system docker)
+  #:use-module (guix build-system)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix build-utils)
+  #:use-module (guix store)
+  #:use-module (guix packages)
+  #:use-module (guix download)
+  #:use-module (guix git-download)
+  #:use-module (guix utils)
+  #:use-module (guix hash)
+  #:use-module (guix build-system)
+  #:use-module (guix build-system gnu)
+  #:use-module (guix build-system trivial)
+  #:use-module (ice-9 match)
+  #:use-module (ice-9 popen)
+  #:export (docker-build-system
+            docker-build))
+
+(define (docker-build-system)
+  "A build system for creating Docker/OCI containers."
+  (build-system
+   (name 'docker)
+   (build-inputs
+    (list (package-inputs (docker-build-system))))
+   (arguments
+    (list
+     #:name "docker-container"
+     #:phases
+     (list
+      (assoc-ref %standard-phases 'configure)
+      (assoc-ref %standard-phases 'build)
+      (assoc-ref %standard-phases 'install)
+      (phase
+       (name "create-container")
+       (description "Create the container image")
+       (build
+        (lambda* (#:key inputs outputs store (system %current-system) #:allow-other-keys)
+          (let* ((output (assoc-ref outputs "out"))
+                 (image-name (or (getenv "IMAGE_NAME") "my-container"))
+                 (image-tag (or (getenv "IMAGE_TAG") "latest"))
+                 (base-image (or (getenv "BASE_IMAGE") "alpine:latest"))
+                 (container-dir (string-append output "/container"))
+                 (dockerfile (string-append container-dir "/Dockerfile")))
+            ;; Create container directory structure
+            (mkdir-p container-dir)
+            
+            ;; Create a simple Dockerfile
+            (call-with-output-file dockerfile
+              (lambda (port)
+                (format port "FROM ~a~%" base-image)
+                (format port "LABEL maintainer=\"Guix Docker Builder\"~%")
+                (format port "WORKDIR /app~%")
+                (format port "COPY . /app~%")
+                (format port "CMD [\"/bin/sh\"]~%")))
+            
+            ;; Build the container image
+            (let ((build-command (string-append "docker build -t " image-name ":" image-tag " " container-dir)))
+              (system* "sh" "-c" build-command))
+            
+            ;; Export the container
+            (let ((export-command (string-append "docker save " image-name ":" image-tag " > " output "/container.tar")))
+              (system* "sh" "-c" export-command))
+            
+            ;; Return the output path
+            output)))))
+     #:outputs '("out")
+     #:implicit-inputs? #f
+     #:modules '((guix build utils))
+     #:imported-modules '((guix build utils))
+     #:properties
+     (list
+      (property 'container-name "docker-container")
+      (property 'container-type 'oci)))))
+
+(define (docker-build name
+                      #:base-image base-image
+                      #:image-name image-name
+                      #:image-tag image-tag
+                      #:inputs inputs
+                      #:phases phases
+                      #:outputs outputs
+                      #:modules modules
+                      #:imported-modules imported-modules
+                      #:properties properties)
+  "Build a Docker/OCI container with the given NAME.
+BASE-IMAGE is the base image to use (default: \"alpine:latest\").
+IMAGE-NAME is the name of the resulting image (default: \"my-container\").
+IMAGE-TAG is the tag for the resulting image (default: \"latest\").
+INPUTS are the build inputs.
+PHASES are the build phases.
+OUTPUTS are the build outputs.
+MODULES are the modules to use.
+IMPORTED-MODULES are the imported modules.
+PROPERTIES are the build properties."
+  (let ((build-system (docker-build-system)))
+    (build-system-build build-system
+                        #:name name
+                        #:base-image base-image
+                        #:image-name image-name
+                        #:image-tag image-tag
+                        #:inputs inputs
+                        #:phases phases
+                        #:outputs outputs
+                        #:modules modules
+                        #:imported-modules imported-modules
+                        #:properties properties)))
+
+;;; docker.scm ends here
